3279d01fe55d1f824de00ec6f4929ec6be80a42e
shanila1986 <shanila.dilshan@gmail.com>
2016-12-15 09:05:54 +0530

    router  info

--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,31 @@
 |
 */
 
+// dashBord page
 Route::get('/', function () {
     return App::make('App\Http\Controllers\BackendController')->dashBoard();
 });
 
+
+// department page
 Route::get('/department', function () {
     return App::make('App\Http\Controllers\BackendController')->department();
 });
 
+
+// insert department 
 Route::get('/addDepartment', function () {
     return App::make('App\Http\Controllers\BackendController')->insertDepartment();
 });
 
+
+// employees page
 Route::get('/employees', function () {
     return App::make('App\Http\Controllers\BackendController')->employees();
 });
 
+
+// insert employees 
 Route::get('/addEmployee', function () {
     return App::make('App\Http\Controllers\BackendController')->insertEmployees();
 });
